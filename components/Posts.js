cd74a29c424714852eb1ba2efa023cf4a9fcf21b
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2021-12-06 19:24:02 +0300

    updated firebase

--- components/Posts.js
+++ components/Posts.js
@@ -2,22 +2,6 @@ import { collection, onSnapshot, orderBy, query } from "@firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
 import Post from "./Post";
-const posts = [
-  {
-    id: "123",
-    username: "ıvır",
-    userImage: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/ocw",
-    caption: "tbt",
-  },
-  {
-    id: "1234",
-    username: "zıvır",
-    userImage: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/ocw",
-    caption: "tbt",
-  },
-];
 
 function Posts() {
   const [posts, setPosts] = useState([]);
--- firebase.js
+++ firebase.js
@@ -5,10 +5,10 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  projectId: "instagram-2-b71f5",
+  storageBucket: "instagram-2-b71f5.appspot.com",
+  messagingSenderId: "946089517222",
+  appId: "1:946089517222:web:f7941b40f35368763dd467",
 };
 
 // Initialize Firebase
